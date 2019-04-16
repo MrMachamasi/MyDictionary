9e2b6bcdc60443a71fce46ce2fbf19598d7e96f2
Softwarica College Lab <softwarica@softwarica.com>
2019-04-16 10:54:33 +0545

    bing

--- app/src/main/java/com/yuskay/mydictionary/MainActivity.java
+++ app/src/main/java/com/yuskay/mydictionary/MainActivity.java
@@ -2,12 +2,33 @@ package com.yuskay.mydictionary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String countries[]={
+            "Nepal",
+            "india",
+            "china",
+            "UK"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView theCountries=findViewById(R.id.myList);
+
+        ArrayAdapter countryAdapter=new ArrayAdapter(
+                MainActivity.this ,
+                android.R.layout.simple_list_item_1,
+                countries
+        );
+
+        theCountries.setAdapter(countryAdapter);
     }
 }
