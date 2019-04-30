737d42b959bed36a0eeb9f13af1f1bd983278c2d
Softwarica College Lab <softwarica@softwarica.com>
2019-04-30 11:08:00 +0545

    bundle and intent

---
+++ app/src/main/java/com/yuskay/mydictionary/Login.java
@@ -0,0 +1,42 @@
+package com.yuskay.mydictionary;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUser();
+            }
+        });
+    }
+
+    private void checkUser() {
+        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+        String password = sharedPreferences.getString("password", "");
+        if (username.equals(etUsername.getText().toString()) && password.equals(etPassword.getText().toString())) {
+            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(Login.this, "Either username or password is incorrect", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
